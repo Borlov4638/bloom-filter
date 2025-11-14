3fb3e5b0e1bb02106e8ce8b2bcd12ec9d5215a0d
boris orlov <borlov@stormwall.pro>
2025-11-14 20:56:40 +0300

    добавил генерацию хэшей

--- internal/filter/hashes.go
+++ internal/filter/hashes.go
@@ -2,15 +2,44 @@ package filter
 
 import (
 	"crypto/sha256"
+	"crypto/sha512"
 )
 
 type HashFunction func(value string) ([]byte, error)
 
-func Sha256(value string) ([]byte, error) {
+func Sha256WithSalt(value, salt string) ([]byte, error) {
 	h := sha256.New()
-	_, err := h.Write([]byte(value))
+	_, err := h.Write([]byte(value + salt))
 	if err != nil {
 		return nil, err
 	}
 	return h.Sum(nil), nil
+}
+
+func Sha512WithSalt(value, salt string) ([]byte, error) {
+	h := sha512.New()
+	_, err := h.Write([]byte(value + salt))
+	if err != nil {
+		return nil, err
+	}
+	return h.Sum(nil), nil
+}
+
+func GetKHashFunctionos(k int) []HashFunction {
+	res := make([]HashFunction, k)
+
+	for i := 0; i < k; i++ {
+		genFunction := func(value string) ([]byte, error) {
+			switch i % 2 {
+			case 0:
+				return Sha256WithSalt(value, string(i))
+			case 1:
+				return Sha512WithSalt(value, string(i))
+			default:
+				return Sha256WithSalt(value, string(i))
+			}
+		}
+		res[i] = genFunction
+	}
+	return res
 }
